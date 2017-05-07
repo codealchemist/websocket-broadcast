0e4f17ad96a80a8f0af51c6847d033c9f584a4de
Alberto Miranda <codealchemist@gmail.com>
2017-05-07 19:09:57 -0300

    Logging and uuid.
    Better logging; added uuid generation; added ascii art.

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 #!/usr/bin/env node
 const WebSocket = require('ws')
 const port = 9090
+const uuid = require('uuid/v1')
+const chalk = require('chalk')
+const fs = require('fs')
+const path = require('path')
 const wss = new WebSocket.Server({
   perMessageDeflate: false,
   port
 }, onListening)
 const clients = []
 
+// print ascii art
+var artFile = path.join(__dirname, './ascii-art.txt')
+var art = fs.readFileSync(artFile, 'utf8')
+console.log(art)
+
 function onListening () {
-  log(`LISTENING on PORT ${port}`)
+  log(chalk.bold(`LISTENING on PORT ${chalk.white(port)}`))
 }
 
 function log () {
-  console.log('[ WEBSOCKET-BROADCAST ]-->', ...arguments)
+  const ts = (new Date()).toISOString()
+  console.log(`${chalk.dim(ts)}:`, ...arguments)
 }
 
 function broadcast (ws, message) {
@@ -22,13 +32,22 @@ function broadcast (ws, message) {
   })
 }
 
+function send (ws, message) {
+  const data = JSON.stringify(message)
+  ws.send(data, (error) => {}) // eslint-disable-line
+}
+
 wss.on('connection', (ws) => {
   const host = ws.upgradeReq.headers.host
-  log('client connected:', host)
+  const id = uuid()
+  log(`NEW client: ID: ${chalk.white(id)} @${chalk.blue(host)}`)
   clients.push(ws)
 
+  // Send UUID to client.
+  send(ws, {type: 'uuid', id})
+
   ws.on('message', (message) => {
-    log(`BROADCAST message from ${host}`, message)
     broadcast(ws, message)
+    log(`MSG from ${host}`, chalk.gray(message))
   })
 })
