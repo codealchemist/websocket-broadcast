b3c9dbb3ce785f3cf5448445478f71753baccf1d
Alberto Miranda <codealchemist@gmail.com>
2018-07-06 11:02:56 -0300

    Added nolog and noid options; port read as param instead of env var; updated readme and ascii art; several command line enhancements; changed default port to 3333.

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ Broadcasts received messages over websockets.
 
 ## How it works
 
-It opens a websockets connection on port 9090.
+It opens a websocket connection on port 3333.
 Clients connected to this server can broadcast messages between them.
-The client who sends a message doesn't receive it back, only the other connected clients get it.
+The client who sends a message doesn't receive it back, it's sent to the other clients only.
 
 ## Client IDs
 
@@ -22,8 +22,20 @@ UUID will be sent in a message with type `uuid`, something like this:
 
 `{type: 'uuid', data: 'ed8ef0b0-35ff-11e7-8de4-6bd9049b7aa7'}`
 
+You can disable ID generation by setting the `--noid` modifier:
+
+`websocket-broadcast --noid`
+
 ## Customize port
 
-To use another port you can start the app like this:
+You can set a custom port by using the `-p` param:
 
-`PORT=8080 websocket-broadcast`
+`websocket-broadcast -p 8080`
+
+## Logging
+
+By default it will log every received message.
+
+If you want to turn off logging you can use the `--nolog`:
+
+`websocket-broadcast --nolog`
---
+++ src/index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+const WebSocket = require('ws')
+const uuid = require('uuid/v1')
+const chalk = require('chalk')
+const fs = require('fs')
+const path = require('path')
+const clear = require('clear')
+const cursor = require('cli-cursor')
+const printii = require('printii')(__dirname)
+const args = require('minimist')(process.argv.slice(2))
+const keyReader = require('./key-reader')
+const port = args.port || args.p || 3333
+const verbose = !args.nolog
+const identify = !args.noid
+
+clear()
+printii()
+cursor.hide()
+keyReader.start()
+
+// Create websocket server.
+const wss = new WebSocket.Server({ port }, onListening)
+
+wss.on('connection', (ws) => {
+  log(`Total clients: ${chalk.white(wss.clients.size)}`)
+
+  if (identify) {
+    const host = ws.upgradeReq.headers.host
+    const id = uuid()
+    if (verbose) log(`NEW client: ID: ${chalk.white(id)} @${chalk.blue(host)}`)
+
+    // Send UUID to client.
+    send(ws, {type: 'uuid', data: id})
+  }
+
+  if (!verbose) return
+  ws.on('message', (message) => {
+    log(`MSG from ${host}`, chalk.gray(message))
+  })
+})
+
+//------------------------------------------------------------------------------
+
+function onListening () {
+  const noidMsg = identify? '' : ` --${chalk.white('noid')}`
+  const nologMsg = verbose? '' : ` --${chalk.white('nolog')}`
+  console.log(chalk.yellow(`LISTENING on PORT ${chalk.white(port)}${noidMsg}${nologMsg}`))
+}
+
+function broadcast (ws, message) {
+  wss.clients.forEach((client) => {
+    if (client === ws) return
+    if (client.readyState !== WebSocket.OPEN) return
+    client.send(message, (error) => {}) // eslint-disable-line
+  })
+}
+
+function send (ws, message) {
+  const data = JSON.stringify(message)
+  if (ws.readyState !== WebSocket.OPEN) return
+  ws.send(data, (error) => {}) // eslint-disable-line
+}
+
+function log () {
+  // Single line basic info.
+  if (!verbose) {
+    const message = Array.prototype.join.call(arguments, ' ')
+    process.stdout.clearLine()
+    process.stdout.write(`\r${message}\r`)
+    return
+  }
+
+  const ts = (new Date()).toISOString()
+  console.log(`${chalk.dim(ts)}:`, ...arguments)
+}
